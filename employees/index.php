a2b2bb72724e910293e9d30023b2ce238d67914f
Ashfame <mail@ashfame.com>
2012-09-20 16:53:28 +0530

    formatting fix

--- employees/index.php
+++ employees/index.php
@@ -1,20 +1,25 @@
 <?php
 
-$employee = array('shawn' => array(
-	'emp_no' => 0001,
-	'firstname' => 'Shawn',
-	'surname' => 'wrangler',
-	'dob' => array(
-		'date' => 9,
-		'month' => 'april',
-		'year' => 1988 ),
-	'age' => 24,
-	'mobile' => '+91-9767544344',
-	'email' => 'shawn@xyz.com',
-	'address' => array(
-		'street no' => '',
-		'state' => 'New Delhi',
-		'country' => 'India',
-		'postal' => 116375 ) ));
+$employee = array(
+	'shawn' => array(
+		'emp_no' => 0001,
+		'firstname' => 'Shawn',
+		'surname' => 'wrangler',
+		'dob' => array(
+			'date' => 9,
+			'month' => 'april',
+			'year' => 1988
+		),
+		'age' => 24,
+		'mobile' => '+91-9767544344',
+		'email' => 'shawn@xyz.com',
+		'address' => array(
+			'street no' => '',
+			'state' => 'New Delhi',
+			'country' => 'India',
+			'postal' => 116375
+		)
+	)
+);
 
-print_r($employee);
+print_r( $employee );
