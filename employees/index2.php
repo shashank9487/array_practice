b0465066a9c3a9e75bf48549fd56774d785bf9ab
shashank <shashank9487@gmail.com>
2012-09-21 17:07:48 +0530

    loop array with random values and grouping array according to ages

--- employees/index2.php
+++ employees/index2.php
@@ -3,26 +3,53 @@
 $first = array( "Neo", "Morpheus", "Trinity", "Cypher", "Tank" );
 $sur = array( "art", "mac", "john", "vipe", "bull" );
 $desig = array( "manager", "tester", "programmer", "designer" );
+$age = array( 20, 25, 30, 40, 44, 50, 66, 65, 70, 80 );
 
-$first_name = array_rand( $first, 5 );
-$sur_name = array_rand( $sur, 5 );
-$desig_name = array_rand( $desig, 4 );
-$num = rand( 20, 99 );
 
+$age = range( 20, 80 );
+$date = range( (date( "d" ) - 20 ), ((date( "d" ) - 20) + 31 ) );
+$month = range( (date( "m" ) - 8 ), ((date( "m" ) - 8) + 11 ) );
+$year = range( 1900, 2000 );
+$emp_num = range( 100, 200 );
 
 $employee = array( );
+
 for ( $i = 0; $i <= 99; $i++ ) {
 
+	$emp_age = array_rand( $age );
+	$get_age = $age[$emp_age];
+
+	$emp_id = array_rand( $emp_num );
+	$get_id = $emp_num[$emp_id];
+
+	$first_name = array_rand( $first );
+	$name = $first[$first_name];
+
+	$sur_name = array_rand( $sur );
+	$sname = $sur[$sur_name];
+
+	$desig_name = array_rand( $desig );
+	$get_desig = $desig[$desig_name];
+
+	$dt = array_rand( $date );
+	$get_date = $date[$dt];
+
+	$mth = array_rand( $month );
+	$get_month = $month[$mth];
+
+	$yr = array_rand( $year );
+	$get_year = $year[$yr];
+
 	$employee[$i] = array(
-		'emp_no' => $num,
-		'firstname' => 'tank',
-		'surname' => 'me',
+		'emp_id' => $get_id,
+		'firstname' => $name,
+		'surname' => $sname,
 		'dob' => array(
-			'date' => 10,
-			'month' => 'may',
-			'year' => 1983
+			'date' => $get_date,
+			'month' => $get_month,
+			'year' => $get_year
 		),
-		'age' => $i,
+		'age' => $get_age,
 		'mobile' => '+91-9765544344',
 		'email' => 'sewdfew@xyz.com',
 		'address' => array(
@@ -31,28 +58,33 @@ for ( $i = 0; $i <= 99; $i++ ) {
 			'country' => 'India',
 			'postal' => 116535
 		),
-		'designation' => 'desg',
+		'designation' => $get_desig,
 		'salary' => '10k'
 	);
 }
 
-//print_r( $data );
+//print_r( $employee );
 
 $youth = array( );
+$experience = array( );
 $retired = array( );
+$sections = array( );
 
 foreach ( $employee as $data ) {
 
-	if ( $data['age'] >= 20 && $data['age'] <= 25 ) {
+	if ( $data['age'] >= 20 && $data['age'] <= 30 ) {
 
-		$youth[$data['age']] = $data;
-	} elseif ( $data['age'] >= 50 && $data['age'] <= 70 ) {
-		$retired[$data['age']] = $data;
+		$youth[$data['age']][] = $data;
+	} elseif ( $data['age'] >= 30 && $data['age'] <= 50 ) {
+		$experience[$data['age']][] = $data;
+	} elseif ( $data['age'] >= 50 && $data['age'] <= 80 ) {
+		$retired[$data['age']][] = $data;
 	}
 }
 
 $sections = array(
 	'youth' => $youth,
+	'experience' => $experience,
 	'retired' => $retired
 );
 
